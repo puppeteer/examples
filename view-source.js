d4dee064d91f7002a39a586c5a0b9f50cdffa91a
Eric Bidelman <ebidel@gmail.com>
2018-02-28 10:45:34 -0800

    view source example

---
+++ view-source.js
@@ -0,0 +1,17 @@
+/**
+ * @author ebidel@ (Eric Bidelman)
+ * License Apache-2.0
+ */
+
+// Curl a page and dump it's markup.
+
+const puppeteer = require('puppeteer');
+
+const URL = process.env.URL || 'https://www.chromestatus.com/features';
+
+puppeteer.launch().then(async browser => {
+  const page = await browser.newPage();
+  const response = await page.goto(URL);
+  console.log(await response.text());
+  await browser.close();
+});
