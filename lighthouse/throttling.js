d8df4bf79b413cc424a1ac8805c22d71b8a74c09
Eric Bidelman <ebidel@gmail.com>
2018-02-28 09:41:06 -0800

    pptr throttling example + lighthouse run

---
+++ lighthouse/throttling.js
@@ -0,0 +1,67 @@
+/**
+ * @author ebidel@ (Eric Bidelman)
+ * License Apache-2.0
+ */
+
+// Combine Puppeteer and Lighthouse to customize the network throttle settings
+// for a Lighthouse audit run. Chrome gets launched by Puppeteer (normally done
+// by LH) and custom network conditions are established through Puppeteer's
+// APIs. Lighthouse then audits the page with those settings.
+//
+// The flow is:
+// 1. Disable Lighthouse's default throttling settings.
+// 2. Launch Chrome using Puppeteer. Tell Lighthouse to reuse that chrome
+//    instance instead of launching it's own.
+// 3. Hand the url to Lighthouse for testing.
+// 4. Puppeteer observes the page opening, then sets up emulation.
+
+const puppeteer = require('puppeteer');
+const lighthouse = require('lighthouse');
+const ReportGenerator = require('lighthouse/lighthouse-core/report/v2/report-generator');
+const fs = require('fs');
+const {URL} = require('url');
+
+(async() => {
+
+const url = 'https://www.chromestatus.com/features';
+
+// Use Puppeteer to launch headless Chrome.
+const browser = await puppeteer.launch({headless: true});
+const remoteDebugPort =(new URL(browser.wsEndpoint())).port;
+
+// Watch for Lighthouse to open url, then customize network conditions.
+// Note: re-establishes throttle settings every time LH reloads the page. Shooooould be ok :)
+browser.on('targetchanged', async target => {
+  const page = await target.page();
+
+  if (page && page.url() === url) {
+    const client = await page.target().createCDPSession();
+    // await client.send('Network.enable'); // Already enabled by pptr.
+    await client.send('Network.emulateNetworkConditions', {
+      offline: false,
+      // values of 0 remove any active throttling. crbug.com/456324#c9
+      latency: 800,
+      downloadThroughput: Math.floor(1.6 * 1024 * 1024 / 8), // 1.6Mbps
+      uploadThroughput: Math.floor(750 * 1024 / 8) // 750Kbps
+    });
+  }
+});
+
+// Lighthouse opens url and tests it.
+// Note: Possible race with Puppeteer observing the tab opening using `targetchanged` above.
+const lhr = await lighthouse(url, {
+  port: remoteDebugPort,
+  output: 'json',
+  logLevel: 'info',
+  disableNetworkThrottling: true,
+  //disableCpuThrottling: true,
+  //disableDeviceEmulation: true,
+});
+
+// Save html report.
+fs.writeFileSync('results.html', new ReportGenerator().generateReportHtml(lhr));
+
+console.log(`Lighthouse score: ${lhr.score}`);
+
+await browser.close();
+})();
