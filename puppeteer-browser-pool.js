86f913c45cb9b423e7de3120240abd60d2aee41c
Rahul kumar <rahulkumar2048@gmail.com>
2018-08-23 10:07:05 +0530

    FIx several code quality issues

--- puppeteer-browser-pool.js
+++ puppeteer-browser-pool.js
@@ -25,43 +25,42 @@
 const genericPool = require('generic-pool');
 const puppeteer = require('puppeteer');
 
-const VIEWPORT = { width: 1028, height: 800, deviceScaleFactor: 2 };
-const HACKER_NEWS_SPA_URL = 'https://hn.algolia.com/';
-const HACKER_NEWS_QUERY_SPA_URL = 'https://hn.algolia.com/?query=angular';
+const VIEWPORT = {width: 1028, height: 800, deviceScaleFactor: 2};
+const HACKER_NEWS_URL = 'https://hn.algolia.com/';
+const HACKER_NEWS_QUERY_URL = `${HACKER_NEWS_URL}?query=chrome`;
 
-
-const factory = {
-    create: async () => {
-        const browser = await puppeteer.launch(puppeteerArgs);
-        const page = await browser.newPage();
-        return page;
-    },
-    destroy: (browser) => {
-        browser.close();
-    },
+const browserPoolFactory = {
+  create: async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    return page;
+  },
+  destroy: (browser) => {
+    return browser.close();
+  },
 };
 
-const browserPool = genericPool.createPool(factory, {
-    max: 5, // maximum number of browser instances that can be created at any given time 
-    min: 2, // minimum number of browser instances to keep in pool at any given time
-    maxWaitingClients: 50
+const browserPool = genericPool.createPool(browserPoolFactory, {
+  max: 5, // maximum number of browser instances that can be created at any given time 
+  min: 2, // minimum number of browser instances to keep in pool at any given time
+  maxWaitingClients: 50
 });
 
 async function getContent(url) {
-    const page = await browserPool.acquire();
-    await page.setViewport(VIEWPORT);
+  const page = await browserPool.acquire();
+  await page.setViewport(VIEWPORT);
 
-    const response = await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
-    // release browser instance back to pool so it can be reused
-    await browserPool.release(page);
-    return response.text();
+  const response = await page.goto(url, {waitUntil: 'networkidle2', timeout: 60000});
+  // release browser instance back to pool so it can be reused
+  await browserPool.release(page);
+  return response.text();
 }
 
 (async () => {
-    const responseHackerNews = await getContent(HACKER_NEWS_SPA_URL);
-    console.log(responseHackerNews);
+  const responseHackerNews = await getContent(HACKER_NEWS_URL);
+  console.log(responseHackerNews);
 
-    // this request will use the instance from the current browser pool resulting in faster response
-    const responseHackerNewsWithQuery = await getContent(HACKER_NEWS_QUERY_SPA_URL);
-    console.log(responseHackerNewsWithQuery);
+  // this request will use the instance from the current browser pool resulting in faster response
+  const responseHackerNewsWithQuery = await getContent(HACKER_NEWS_QUERY_URL);
+  console.log(responseHackerNewsWithQuery);
 })();
